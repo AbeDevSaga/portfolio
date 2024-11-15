f2175b3391f66599e8466b1a36c78c2e5c5bb1da
Adem Ahmed Bekar <109433979+ademahmed23@users.noreply.github.com>
2024-11-16 00:50:01 +0300

    Changed

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -40,12 +40,12 @@ type TConfig = {
 
 export const config: TConfig = {
   html: {
-    title: "John Doe — 3D Portfolio",
-    fullName: "John Doe",
-    email: "johndoe@mail.com",
+    title: "Adem Ahmed— 3D Portfolio",
+    fullName: "Adem Ahmed Bekar",
+    email: "ademahmedbekr@gmail.com",
   },
   hero: {
-    name: "John Doe",
+    name: "Adem Ahmed",
     p: ["I develop 3D visuals, user", "interfaces and web applications"],
   },
   contact: {
