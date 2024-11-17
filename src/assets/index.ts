a61225be610799aa63797bb5b184f2c9ce14eed6
Adem Ahmed Bekar <109433979+ademahmed23@users.noreply.github.com>
2024-11-17 22:13:39 +0300

    Update index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -26,7 +26,7 @@ import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
-import hinta from "./company/hinta.png"
+import hinta from "./company/hinta.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
