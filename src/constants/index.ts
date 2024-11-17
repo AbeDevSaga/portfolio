2036ca10fc0856754d94db6910bf325a3f5e96e3
Adem Ahmed Bekar <109433979+ademahmed23@users.noreply.github.com>
2024-11-17 23:29:24 +0300

    h

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -34,6 +34,7 @@ import {
   jobit,
   tripguide,
   project4,
+  hintas,
   threejs,
 } from "../assets";
 
@@ -294,6 +295,27 @@ const projects: TProject[] = [
     name: "School Managment System",
     description:
       "A web-based platform that enables users to manage, track, and streamline school operations, including student information, attendance, grades, and communication, offering a seamless and efficient solution for educational administration.",
+      tags: [
+      {
+        name: "Laravel",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Blade",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: project4,
+    sourceCodeLink: "https://github.com/",
+  },
+  {
+    name: "Harari Technology Agency",
+    description:
+      "A web-based platform that enables users to explore, access, and engage with Harari Innovation and Technology Agency's programs, projects, and resources, offering a seamless and efficient solution for fostering innovation and technological advancement.",
       tags: [
       {
         name: "Laravel",
