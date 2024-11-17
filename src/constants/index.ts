b6b6d3d0bd2a021155e46e0852dcc8083ce63031
Adem Ahmed Bekar <109433979+ademahmed23@users.noreply.github.com>
2024-11-17 22:07:31 +0300

    css

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -169,6 +169,21 @@ const experiences: TExperience[] = [
       "Participating in code reviews and providing constructive feedback to other developers.",
     ],
   },
+
+  {
+    title: "Web Developer",
+    companyName: "HInTA",
+    icon: shopify,
+    iconBg: "#383E56",
+    date: "Jan 2022 - Jan 2023",
+    points: [
+      "Developing and maintaining web applications using Laravel and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+
   {
     title: "Full stack Developer",
     companyName: "Meta",
