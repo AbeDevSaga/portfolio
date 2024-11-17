d563ba53d87a0d3de76a4bfbd0d2b969b5456412
Adem Ahmed Bekar <109433979+ademahmed23@users.noreply.github.com>
2024-11-17 21:13:16 +0300

    changed

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -284,7 +284,7 @@ const projects: TProject[] = [
         color: "blue-text-gradient",
       },
       {
-        name: "Blae",
+        name: "Blade",
         color: "green-text-gradient",
       },
       {
